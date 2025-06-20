a28be6cbe41c79402933f63d0bc7f86cadbcaec5
Appiah Gyimah Maxwell <maxappiahofficial@gmail.com>
2025-06-20 00:45:23 +0000

    remove env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,6 +5,8 @@ const environment = import.meta.env;
 
 export const variables = {
   "something": environment.VITE_SOMETHING,
-  formspree: "https://formspree.io/f/xwpkjvle"
+  formspree: "https://formspree.io/f/xwpkjvle",
+  SUPABASE_URL: environment.SUPABASE_URL,
+  SUPABASE_ANON_KEY: environment.SUPABASE_ANON_KEY,
 
 };
