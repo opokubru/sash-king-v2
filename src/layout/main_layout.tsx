ad2397812e0379fbcda94227a10164660e5dc5b9
Appiah Gyimah Maxwell <maxappiahofficial@gmail.com>
2025-09-04 21:19:45 +0000

    show storefonrt

--- src/layout/main_layout.tsx
+++ src/layout/main_layout.tsx
@@ -1,12 +1,12 @@
 import LazyPageWrapper from './lazy.page.wrapper';
 import Footer from '@/components/shared/footer';
-// import NavbarComponent from '@/components/shared/navbar';
+import NavbarComponent from '@/components/shared/navbar';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <main>
-      {/* <NavbarComponent /> */}
+      <NavbarComponent />
       <div className=" bg-[rgba(197,195,195,0.165)]">
         <LazyPageWrapper component={<Outlet />} />
       </div>
